385a5cff150baddf7b765ece8810a8a5d30f5ead
Park <pbk95120@gmail.com>
2024-04-29 16:32:39 +0900

    [Fix] jest config fix

--- frontend/src/test/App.test.tsx
+++ frontend/src/test/App.test.tsx
@@ -1,11 +1,11 @@
-import App from '@/pages/TestPage';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'; // jest-dom의 확장 기능을 사용하기 위해 임포트
+import '@testing-library/jest-dom';
+import ChatPage from '@/pages/ChatPage';
 
-describe('App 컴포넌트', () => {
-  test('h1 태그에 "Hello world!" 텍스트가 포함되어 있는지 확인', () => {
-    render(<App />);
-    const headingElement = screen.getByText(/hello world!/i); // 대소문자를 구분하지 않고 텍스트 검색
-    expect(headingElement).toBeInTheDocument(); // 해당 엘리먼트가 문서에 존재하는지 확인
+describe('ChatPage 컴포넌트', () => {
+  test('페이지 컴포넌트가 정상적으로 렌더링 되는지', () => {
+    render(<ChatPage />);
+    const divElement = screen.getByText('페이지');
+    expect(divElement).toBeInTheDocument();
   });
 });
