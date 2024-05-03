f593bb911067c638d7b1d6d78722e19c24c83b76
leehoosgg <leehoosgg@gmail.com>
2024-05-03 15:13:24 +0900

    [Feat] proceedLogout

---
+++ backend/src/tests/proceedLogout.test.ts
@@ -0,0 +1,18 @@
+import { issueToken } from '@utils/issueToken';
+import request from 'supertest';
+import app from '../app';
+
+describe('POST /logout 로그아웃 요청', () => {
+  it('access-token 제거', async () => {
+    const token = issueToken('email');
+    const res = await request(app).post('/logout').set('Set-Cookie', `access-token=${token}`);
+    const cookie = res.headers['set-cookie'][0];
+    expect(cookie).toBeDefined();
+
+    const regex = /access-token=([^;]*)/;
+    const match = cookie.match(regex);
+    if (match && match[1].trim() === '') {
+      expect(true).toBe(true);
+    } else fail();
+  });
+});
