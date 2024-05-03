f7abb1e85795b8d381f2c136e0cee038ac193780
Park <pbk95120@gmail.com>
2024-05-03 14:05:44 +0900

    [Feat] chat message form update

--- frontend/src/components/chat/ChatTextarea.tsx
+++ frontend/src/components/chat/ChatTextarea.tsx
@@ -6,6 +6,7 @@ import { RiGalleryLine } from 'react-icons/ri';
  */
 const ChatTextarea = () => {
   const [uploadImg, setUploadImg] = useState<string>();
+  const [message, setMessage] = useState<string>('');
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
@@ -15,15 +16,32 @@ const ChatTextarea = () => {
     setUploadImg(imageUrl);
   };
 
+  const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log(uploadImg);
+    console.log(message);
     console.log('submit');
+
+    setUploadImg('');
+    setMessage('');
+
     // form data post
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey && e.nativeEvent.isComposing === false) {
+      e.preventDefault();
+      handleSubmit(e as any);
+    }
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         {uploadImg && <img src={'https://placehold.co/60x60'} alt='Thumbnail' className='mx-6 mb-2 rounded-lg' />}
         <div className='relative flex items-center border border-gray mx-6 rounded-xl min-h-[2.375rem] min-w-[20.425rem]'>
           {!uploadImg && (
@@ -42,14 +60,11 @@ const ChatTextarea = () => {
             rows={1}
             className='pl-2 w-8/12 h[2.25rem] text-xs resize-none focus:outline-none focus:ring-0 overflow-hidden'
             placeholder='메세지 입력...'
+            value={message}
+            onChange={handleMessageChange}
+            onKeyDown={handleKeyDown}
           ></textarea>
-          <button
-            type='submit'
-            className='absolute right-0 w-14 h-[2.4rem] bg-pastelred text-white text-xs rounded-xl'
-            onClick={() => handleSubmit}
-          >
-            전송
-          </button>
+          <button className='absolute right-0 w-14 h-[2.4rem] bg-pastelred text-white text-xs rounded-xl'>전송</button>
         </div>
       </form>
     </div>
