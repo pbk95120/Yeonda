0290dc7a6033a2784c197a898af03330586f5590
leehoosgg <leehoosgg@gmail.com>
2024-05-07 16:25:08 +0900

    [Hotfix] typo in proceddLogout.test

--- backend/src/tests/proceedLogout.test.ts
+++ backend/src/tests/proceedLogout.test.ts
@@ -5,7 +5,7 @@ import request from 'supertest';
 describe('POST /logout 로그아웃 요청', () => {
   it('access-token 제거', async () => {
     const token = issueToken('email');
-    const res = await request(app).post('/logout').set('Set-Cookie', `access-token=${token}`);
+    const res = await request(app).post('/logout').set('Cookie', `access-token=${token}`);
     const cookie = res.headers['set-cookie'][0];
     expect(cookie).toBeDefined();
 
