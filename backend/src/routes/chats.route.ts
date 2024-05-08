1946d0fe1280eed1ab1eec4ec0950eabf7017e9f
BIG-blueshark <blueshark.1949@gmail.com>
2024-05-08 16:57:57 +0900

    [Fix] chatlist route add controllerWrapper

--- backend/src/routes/chats.route.ts
+++ backend/src/routes/chats.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { getChatlist, deleteRelationship } from '@controllers/chats.controller';
 import { authenticateUser } from '@middlewares/authenticateUser';
+import { controllerWrapper } from '@middlewares/controllerWrapper';
 const ChatsRoute = express.Router();
 ChatsRoute.use(express.json());
 
-ChatsRoute.get('/', authenticateUser, getChatlist);
-ChatsRoute.delete('/:user2_id', authenticateUser, deleteRelationship);
+ChatsRoute.get('/', authenticateUser, controllerWrapper(getChatlist));
+ChatsRoute.delete('/:user2_id', authenticateUser, controllerWrapper(deleteRelationship));
 
 export default ChatsRoute;
