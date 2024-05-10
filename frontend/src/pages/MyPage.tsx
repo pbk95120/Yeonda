77bd6c099f1285c81c08e4b0e4aed9beb42a9033
UupDownPark <geakura123@gmail.com>
2024-05-10 14:59:44 +0900

    [fix] branch fix

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -8,19 +8,28 @@ import SVG from '@/assets/images/Profile.svg?react';
 import { IoFemale } from 'react-icons/io5';
 import { IoMale } from 'react-icons/io5';
 import { useState } from 'react';
+import Tags from '@/components/join/Tags';
+import { dividerClasses } from '@mui/material';
+
+const tags = ['롤토체스', '농구', '게임', '우주파괴', '취뽀'];
 
 const MyPage = () => {
-  const [gender, setGender] = useState<'male' | 'female'>('female');
+  const [gender, setGender] = useState<'male' | 'female'>('male');
   return (
-    <div className='flex flex-col items-center'>
+    <div className='flex flex-col items-center space-y-2'>
       <SVG className='h-[221px] w-[230px] m-2' />
-      <div className='font-sans font-bold'>킹상하</div>
-      <div className='flex flex-row justify-center items-center'>
-        <div className='font-sans text-lightgray'>32세</div>
+      <div className='font-sans font-bold text-xl'>킹상하</div>
+      <div className='flex flex-row justify-center items-center space-x-2'>
+        <div className='font-sans text-lightgray text-xl'>32세</div>
         <div className={gender === 'male' ? 'text-blue' : 'text-pastelpeach'}>
           {gender === 'male' ? <IoMale className='w-5 h-5' /> : <IoFemale className='w-5 h-5' />}
-          <Input inputFor='default' />
         </div>
+      </div>
+      <div className='font-sans font-bold text-xl'>대구</div>
+      <div id='tag container'>
+        {tags.map((tag, i) => (
+          <div></div>
+        ))}
       </div>
     </div>
   );
