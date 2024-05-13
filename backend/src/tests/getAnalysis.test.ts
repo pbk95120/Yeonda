a13ba4ede747ad3daf6403b106ef821971e9a659
BIG-blueshark <blueshark.1949@gmail.com>
2024-05-13 18:46:51 +0900

    [Fix] admin 테스트 파일 경로 수정

--- backend/src/tests/getAnalysis.test.ts
+++ backend/src/tests/getAnalysis.test.ts
@@ -1,5 +1,5 @@
 import { analysisSchema } from '@schemas/admin.schema';
-import app from '@src/app';
+import {server} from '@src/app';
 import Database from '@src/db';
 import { issueToken } from '@src/utils/issueToken';
 import http from 'http-status-codes';
@@ -15,12 +15,12 @@ afterAll(async () => {
 
 describe('Get /admin/user/analysis 관리자 이용자 분석', () => {
   it('관리자 계정 비정상 접근', async () => {
-    const response = await request(app).get('/admin/user/analysis');
+    const response = await request(server).get('/admin/user/analysis');
     expect(response.status).toBe(http.UNAUTHORIZED);
   });
   it('오늘 여성, 남성 유저수, 2주 이내 일기 작성하지 않은 유저 목록', async () => {
     const token = issueToken('admin');
-    const response = await request(app).get('/admin/user/analysis').set('Cookie', `access-token=${token}`);
+    const response = await request(server).get('/admin/user/analysis').set('Cookie', `access-token=${token}`);
     expect(response.status).toBe(http.OK);
     const { error } = analysisSchema.validate(response.body);
     expect(error).toBeUndefined();
--- backend/src/tests/getStatistic.test.ts
+++ backend/src/tests/getStatistic.test.ts
@@ -1,5 +1,5 @@
 import { statisticSchema } from '@schemas/admin.schema';
-import app from '@src/app';
+import { server } from '@src/app';
 import Database from '@src/db';
 import { issueToken } from '@src/utils/issueToken';
 import http from 'http-status-codes';
@@ -15,12 +15,12 @@ afterAll(async () => {
 
 describe('Get /admin/user/statistic 관리자 이용자 통계', () => {
   it('관리자 계정 비정상 접근', async () => {
-    const response = await request(app).get('/admin/user/statistic');
+    const response = await request(server).get('/admin/user/statistic');
     expect(response.status).toBe(http.UNAUTHORIZED);
   });
   it('오늘 index, 일주일 (다이어리, 매칭, 유저) 수', async () => {
     const token = issueToken('admin');
-    const response = await request(app).get('/admin/user/statistic').set('Cookie', `access-token=${token}`);
+    const response = await request(server).get('/admin/user/statistic').set('Cookie', `access-token=${token}`);
     expect(response.status).toBe(http.OK);
     const { error } = statisticSchema.validate(response.body);
     expect(error).toBeUndefined();
