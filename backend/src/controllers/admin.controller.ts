dca08f6cfc5c525a52af2c0b92679c2ad125403c
BIG-blueshark <blueshark.1949@gmail.com>
2024-05-13 18:40:26 +0900

    [Feat] admin 관리자 이용자 분석

--- backend/src/controllers/admin.controller.ts
+++ backend/src/controllers/admin.controller.ts
@@ -1,4 +1,5 @@
 import { selectDiaryCount, selectmatchingCount, selectuserCount } from '@databases/getStatistic.database';
+import { selectGenderCount, selectAverageDiary } from '@databases/getAnalysis.database';
 import { databaseConnector } from '@middlewares/databaseConnector';
 import { Controller } from '@schemas/controller.schema';
 import http from 'http-status-codes';
@@ -21,6 +22,20 @@ export const getStatistic: Controller = async (req, res) => {
     weekly_diary_count: weekly_diary_count,
     weekly_matching_count: weekly_matching_count,
     weekly_user_count: weekly_user_count,
+  };
+  res.status(http.OK).json(result);
+};
+
+export const getAnalysis: Controller = async (req, res) => {
+  if (req.body.email !== 'admin') throw new CustomError(http.UNAUTHORIZED, '잘못된 접근');
+
+  const gender_count = await databaseConnector(selectGenderCount)();
+  const average_diary = await databaseConnector(selectAverageDiary)();
+
+  const result = {
+    male_count: gender_count[0].male_count,
+    female_count: gender_count[0].female_count,
+    average_diary: average_diary,
   };
   res.status(http.OK).json(result);
 };
--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getStatistic } from '@controllers/admin.controller';
+import { getStatistic, getAnalysis } from '@controllers/admin.controller';
 import { authenticateUser } from '@middlewares/authenticateUser';
 import { controllerWrapper } from '@middlewares/controllerWrapper';
 const AdminRoute = express.Router();
 AdminRoute.use(express.json());
 
 AdminRoute.get('/statistic', controllerWrapper(authenticateUser, getStatistic));
+AdminRoute.get('/analysis', controllerWrapper(authenticateUser, getAnalysis));
 
 export default AdminRoute;
