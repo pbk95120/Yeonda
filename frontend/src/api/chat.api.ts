563aa9109e181fcec1357b3e9d7c9b60791595f1
Park <pbk95120@gmail.com>
2024-05-13 16:38:51 +0900

    [Feat] update chat delete api

--- frontend/src/api/chat.api.ts
+++ frontend/src/api/chat.api.ts
@@ -7,3 +7,11 @@ import { requestHandler } from '@/api/http';
 export const fetchChatList = async () => {
   return await requestHandler('get', '/chatlist');
 };
+
+/**
+ * 채팅삭제 DELETE API
+ * @param userDate
+ */
+export const deleteChat = async (userId: number) => {
+  return await requestHandler('delete', `/likes/${userId}`);
+};
