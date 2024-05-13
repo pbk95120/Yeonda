025090eafde41177647d7387a0ccccaa45635a77
Dev-Lee-js <whdtlr202@gmail.com>
2024-05-13 23:08:46 +0900

    frontend merge

--- backend/src/utils/getUserIdByEmail.ts
+++ backend/src/utils/getUserIdByEmail.ts
@@ -1,9 +1,12 @@
+import CustomError from '@src/error';
 import { RowDataPacket } from 'mysql2';
 import { Connection } from 'mysql2/promise';
+import { http } from 'winston';
 
 export const getUserIdByEmail = async (conn: Connection, email: string): Promise<number> => {
   const sql = 'select id from users where email =:email';
   const values = { email: email };
   const [result] = await conn.execute<RowDataPacket[]>(sql, values);
-  return result[0].id;
+  if (result[0]) return result[0].id;
+  else throw new CustomError(http.NOT_FOUND, '존재하지 않는 사용자');
 };
