e461d5d0c2f33b775fbb445673bd4e384e32acf8
BIG-blueshark <blueshark.1949@gmail.com>
2024-05-15 08:03:30 +0900

    [Feat] diary 내일기 상세

--- backend/src/controllers/myDiary.controller.ts
+++ backend/src/controllers/myDiary.controller.ts
@@ -1,4 +1,5 @@
 import { selectMyDiary } from '@databases/selectMyDiary.database';
+import { selectMyDiaryDetail } from '@databases/selectMyDiaryDetail.database';
 import { databaseConnector } from '@middlewares/databaseConnector.middleware';
 import { Controller } from '@schemas/controller.schema';
 import http from 'http-status-codes';
@@ -6,6 +7,20 @@ import http from 'http-status-codes';
 export const getMyDiary: Controller = async (req, res) => {
   const MyDiary = await databaseConnector(selectMyDiary)(parseInt(req.body.user_id));
   const transTags = MyDiary.map((item) => {
+    return {
+      ...item,
+      tags: JSON.parse(item.tags),
+    };
+  });
+  res.status(http.OK).json(transTags);
+};
+
+export const getMyDiaryDetail: Controller = async (req, res) => {
+  const MyDiaryDetail = await databaseConnector(selectMyDiaryDetail)(
+    parseInt(req.body.user_id),
+    parseInt(req.params.id),
+  );
+  const transTags = MyDiaryDetail.map((item) => {
     return {
       ...item,
       tags: JSON.parse(item.tags),
--- backend/src/routes/myDiary.route.ts
+++ backend/src/routes/myDiary.route.ts
@@ -1,9 +1,10 @@
-import { getMyDiary } from '@controllers/myDiary.controller';
+import { getMyDiary, getMyDiaryDetail } from '@controllers/myDiary.controller';
 import { authenticateUser } from '@middlewares/authenticateUser.middleware';
 import { controllerWrapper } from '@middlewares/controllerWrapper.middleware';
 import express from 'express';
 const MyDiaryRoute = express.Router();
 
 MyDiaryRoute.get('/', controllerWrapper(authenticateUser, getMyDiary));
+MyDiaryRoute.get('/:id', controllerWrapper(authenticateUser, getMyDiaryDetail));
 
 export default MyDiaryRoute;
