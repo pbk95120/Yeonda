acc44ff0c286ea78d261ff83e5e078a239101ef4
BIG-blueshark <blueshark.1949@gmail.com>
2024-05-14 22:38:40 +0900

    [Feat] diary 프로필 남의 일기 상세

--- backend/src/controllers/yourDiary.controller.ts
+++ backend/src/controllers/yourDiary.controller.ts
@@ -1,4 +1,5 @@
 import { selectYourDiary } from '@databases/selectYourDiary.database';
+import { selectYourDiaryDetail } from '@databases/selectYourDiaryDetail.database';
 import { databaseConnector } from '@middlewares/databaseConnector.middleware';
 import { Controller } from '@schemas/controller.schema';
 import http from 'http-status-codes';
@@ -6,6 +7,20 @@ import http from 'http-status-codes';
 export const getYourDiary: Controller = async (req, res) => {
   const yourDiary = await databaseConnector(selectYourDiary)(parseInt(req.params.id));
   const transTags = yourDiary.map((item) => {
+    return {
+      ...item,
+      tags: JSON.parse(item.tags),
+    };
+  });
+  res.status(http.OK).json(transTags);
+};
+
+export const getYourDiaryDetail: Controller = async (req, res) => {
+  const yourDiaryDetail = await databaseConnector(selectYourDiaryDetail)(
+    parseInt(req.params.id),
+    parseInt(req.params.diary_id),
+  );
+  const transTags = yourDiaryDetail.map((item) => {
     return {
       ...item,
       tags: JSON.parse(item.tags),
