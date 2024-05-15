805ba768cfa7978877ff3f17d1c0e792c319ba4a
leehoosgg <leehoosgg@gmail.com>
2024-05-15 20:02:25 +0900

    [Fix] modify decodeLogonFromToken as verify

--- backend/src/utils/decodeLogonFromToken.ts
+++
@@ -1,8 +0,0 @@
-import { Logon } from '@schemas/login.schema';
-import 'dotenv/config';
-import jwt from 'jsonwebtoken';
-const { JWT_SECRET } = process.env;
-
-export const decodeLogonFromToken = (token: string): Logon => {
-  return jwt.decode(token) as Logon;
-};
