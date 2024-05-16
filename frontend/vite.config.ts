5ad5bc84fd6356075c242880f1345e39b7f9acf9
geun99 <lich1115@naver.com>
2024-05-16 18:56:46 +0900

    [refactor] 약간의 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,18 @@
-import * as path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ include: "**/*.svg?react" }), tsconfigPaths()],
+  plugins: [react(), svgr({ include: '**/*.svg?react' }), tsconfigPaths()],
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
