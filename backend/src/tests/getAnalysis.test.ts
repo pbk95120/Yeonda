130a4950ad4f9a5212de0504e7aafd0e51b656b0
BIG-blueshark <blueshark.1949@gmail.com>
2024-05-16 14:45:10 +0900

    [Refactor] Admin 관리자 이용자 통계, 분석

--- backend/src/tests/getAnalysis.test.ts
+++ backend/src/tests/getAnalysis.test.ts
@@ -4,6 +4,7 @@ import Database from '@src/db';
 import { issueAccessToken } from '@src/utils/issueToken';
 import http from 'http-status-codes';
 import request from 'supertest';
+import 'dotenv/config';
 
 beforeAll(async () => {
   Database.switchToTest();
@@ -14,16 +15,16 @@ afterAll(async () => {
 });
 
 describe('Get /admin/user/analysis 관리자 이용자 분석', () => {
-  it('관리자 계정 비정상 접근', async () => {
-    const response = await request(server).get('/admin/user/analysis');
-    expect(response.status).toBe(http.UNAUTHORIZED);
-  });
-
   it('오늘 여성, 남성 유저수, 2주 이내 일기 작성하지 않은 유저 목록', async () => {
-    const token = issueAccessToken('admin');
+    const token = issueAccessToken(parseInt(process.env.ADMIN_ID), process.env.ADMIN_EMAIL);
     const response = await request(server).get('/admin/user/analysis').set('Cookie', `access-token=${token}`);
     expect(response.status).toBe(http.OK);
     const { error } = analysisSchema.validate(response.body);
     expect(error).toBeUndefined();
+  });
+
+  it('관리자 계정 비정상 접근', async () => {
+    const response = await request(server).get('/admin/user/analysis');
+    expect(response.status).toBe(http.UNAUTHORIZED);
   });
 });
--- backend/src/tests/getStatistic.test.ts
+++ backend/src/tests/getStatistic.test.ts
@@ -4,6 +4,7 @@ import Database from '@src/db';
 import { issueAccessToken } from '@src/utils/issueToken';
 import http from 'http-status-codes';
 import request from 'supertest';
+import 'dotenv/config';
 
 beforeAll(async () => {
   Database.switchToTest();
@@ -14,16 +15,16 @@ afterAll(async () => {
 });
 
 describe('Get /admin/user/statistic 관리자 이용자 통계', () => {
-  it('관리자 계정 비정상 접근', async () => {
-    const response = await request(server).get('/admin/user/statistic');
-    expect(response.status).toBe(http.UNAUTHORIZED);
-  });
-
-  it('오늘 index, 일주일 (다이어리, 매칭, 유저) 수', async () => {
-    const token = issueAccessToken('admin');
+  it('오늘 날짜 index, 일주일의 (다이어리, 매칭, 유저) 수', async () => {
+    const token = issueAccessToken(parseInt(process.env.ADMIN_ID), process.env.ADMIN_EMAIL);
     const response = await request(server).get('/admin/user/statistic').set('Cookie', `access-token=${token}`);
     expect(response.status).toBe(http.OK);
     const { error } = statisticSchema.validate(response.body);
     expect(error).toBeUndefined();
+  });
+
+  it('관리자 계정 비정상 접근', async () => {
+    const response = await request(server).get('/admin/user/statistic');
+    expect(response.status).toBe(http.UNAUTHORIZED);
   });
 });
