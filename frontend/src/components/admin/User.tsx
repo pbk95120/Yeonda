61b2b5727ad964f5f56808d2e27d658ceca78b7a
geun99 <lich1115@naver.com>
2024-05-17 21:28:55 +0900

    [feat] admin 페이지 구현 완료

--- frontend/src/components/admin/User.tsx
+++ frontend/src/components/admin/User.tsx
@@ -3,14 +3,10 @@ interface UserProps {
   email: string;
 }
 
-const User: React.FC<UserProps> = ({ profilePictureUrl, email }) => {
+const User = ({ profilePictureUrl, email }: UserProps) => {
   return (
-    <div className='border border-black rounded-lg p-4 flex items-center m-4'>
-      <img
-        src='https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c3f9ef29649575.55fcf0a585830.jpg'
-        alt={profilePictureUrl}
-        className='rounded-xl w-12 h-12 mr-4'
-      />
+    <div className='m-4 flex items-center rounded-lg border border-black p-4'>
+      <img src={profilePictureUrl} alt={profilePictureUrl} className='mr-4 h-12 w-12 rounded-xl' />
       <div>{email}</div>
     </div>
   );
