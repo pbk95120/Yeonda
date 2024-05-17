9855e10930e997e5fbd897a915ef8a2560b2d6b4
UupDownPark <geakura123@gmail.com>
2024-05-17 23:51:50 +0900

    [feat] mypage.api.ts

--- frontend/src/api/mypage.api.ts
+++ frontend/src/api/mypage.api.ts
@@ -4,6 +4,30 @@ export const getMyPage = async () => {
   return await requestHandler('get', '/profile/my');
 };
 
-export const withDrawl = async () => {
-  return await requestHandler('delete');
+export const withDrawal = async () => {
+  return await requestHandler('delete', '/signout');
+};
+
+export const getMyPageMyInfo = async () => {
+  return await requestHandler('get', '/profile/my/setting');
+};
+
+export const patchMyInfoPicture = async () => {
+  return await requestHandler('patch', '/profile/my/setting/picture');
+};
+
+export const getMyPageMyPref = async () => {
+  return await requestHandler('get', '/profile/my/preference');
+};
+
+export const patchMyPageMyPref = async () => {
+  return await requestHandler('patch', '/profile/my/preference');
+};
+
+export const getMyTag = async () => {
+  return await requestHandler('get', '/profile/my/tag');
+};
+
+export const putMyTag = async () => {
+  return await requestHandler('put', '/profile/my/tag');
 };
