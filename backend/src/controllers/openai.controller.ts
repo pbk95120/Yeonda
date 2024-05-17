6c26e382d5b27a7c2f3808ed1e7d4a8bc3a54257
leehoosgg <leehoosgg@gmail.com>
2024-05-17 21:27:54 +0900

    [Fix] add auth admin procedure on create tag

--- backend/src/controllers/openai.controller.ts
+++ backend/src/controllers/openai.controller.ts
@@ -1,5 +1,6 @@
 import { insertTag } from '@databases/openai/insertTag.database';
 import { databaseConnector } from '@middlewares/databaseConnector.middleware';
+import { adminSchema } from '@schemas/admin.schema';
 import { Controller } from '@schemas/controller.schema';
 import { CreateTagSchema } from '@schemas/openai.schema';
 import CustomError from '@src/error';
@@ -9,7 +10,10 @@ import OpenAI from 'openai';
 const openai = new OpenAI();
 
 export const createTag: Controller = async (req, res) => {
-  const { error } = CreateTagSchema.validate(req.body);
+  let { error } = adminSchema.validate({ user_id: req.body.user_id, email: req.body.email });
+  if (error) throw new CustomError(http.UNAUTHORIZED, '관리자만 접근 가능', error);
+
+  { error } = CreateTagSchema.validate(req.body);
   if (error) throw new CustomError(http.BAD_REQUEST, '잘못된 태그 생성 요청 양식', error);
 
   const tag = req.body.tag;
--- backend/src/routes/openai.route.ts
+++ backend/src/routes/openai.route.ts
@@ -1,8 +1,9 @@
 import { createTag } from '@controllers/openai.controller';
+import { authenticateUser } from '@middlewares/authenticateUser.middleware';
 import { controllerWrapper } from '@middlewares/controllerWrapper.middleware';
 import express from 'express';
 const OpenAIRoute = express.Router();
 
-OpenAIRoute.post('/embedding/tag', controllerWrapper(createTag));
+OpenAIRoute.post('/embedding/tag', controllerWrapper(authenticateUser, createTag));
 
 export default OpenAIRoute;
