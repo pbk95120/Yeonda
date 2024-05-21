c91e4f9b207acac2a6cafc5979dbc05a4f30ac89
leehoosgg <leehoosgg@gmail.com>
2024-05-21 18:48:50 +0900

    [Feat] deleteTag
    also style, refactor admin codes

--- backend/src/routes/openai.route.ts
+++
@@ -1,9 +0,0 @@
-import { createTag } from '@controllers/openai.controller';
-import { authenticateUser } from '@middlewares/authenticateUser.middleware';
-import { controllerWrapper } from '@middlewares/controllerWrapper.middleware';
-import express from 'express';
-const OpenAIRoute = express.Router();
-
-OpenAIRoute.post('/embedding/tag', controllerWrapper(authenticateUser, createTag));
-
-export default OpenAIRoute;
