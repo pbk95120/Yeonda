8af56409a0b86b54fd831abab8124afdc12ce360
BIG-blueshark <blueshark.1949@gmail.com>
2024-05-23 14:44:20 +0900

    [Fix] yourDiary 태그 id / name으로 수정

--- backend/src/databases/yourDiary/selectYourDiary.database.ts
+++ backend/src/databases/yourDiary/selectYourDiary.database.ts
@@ -9,17 +9,20 @@ export const selectYourDiary = async (
 ): Promise<yourDiaryResults[]> => {
   const offset = (currentPage - 1) * limit;
   const sql = `SELECT 
-        d.id, u.nickname, u.picture_url, d.title, d.content, d.created_at, d.likes, JSON_ARRAYAGG(dt.tag_id) AS tags
+        d.id, u.nickname, u.picture_url, d.title, d.content, d.created_at, d.likes, 
+        json_arrayagg(json_object('id', t.id, 'name', t.name)) as tags
     FROM
         user u
     JOIN
         diary d ON u.id = d.user_id
     LEFT JOIN
         diary_tag dt ON d.id = dt.diary_id
+    LEFT JOIN
+        tag t ON dt.tag_id = t.id
     WHERE
         u.id = :your_id
     GROUP BY
-        d.id
+        d.id, u.nickname, u.picture_url, d.title, d.content, d.created_at, d.likes
     ORDER BY
         d.created_at DESC
     LIMIT 
@@ -28,5 +31,6 @@ export const selectYourDiary = async (
 
   const values = { your_id: your_id, limit: limit, offset: offset };
   const [result] = await conn.query(sql, values);
+
   return result as yourDiaryResults[];
 };
--- backend/src/databases/yourDiary/selectYourDiaryDetail.database.ts
+++ backend/src/databases/yourDiary/selectYourDiaryDetail.database.ts
@@ -7,20 +7,24 @@ export const selectYourDiaryDetail = async (
   your_id: number,
 ): Promise<yourDiaryResults[]> => {
   const sql = `SELECT 
-        d.id, u.nickname, u.picture_url, d.title, d.content, d.created_at, d.likes, JSON_ARRAYAGG(dt.tag_id) AS tags
+        d.id, u.nickname, u.picture_url, d.title, d.content, d.created_at, d.likes, 
+        COALESCE(JSON_ARRAYAGG(JSON_OBJECT('id', t.id, 'name', t.name)), JSON_ARRAY()) as tags
     FROM
         user u
     JOIN
         diary d ON u.id = d.user_id
     LEFT JOIN
         diary_tag dt ON d.id = dt.diary_id
+    LEFT JOIN
+        tag t ON dt.tag_id = t.id
     WHERE
         u.id = :your_id AND d.id = :diary_id
     GROUP BY
-        d.id
+        d.id, u.nickname, u.picture_url, d.title, d.content, d.created_at, d.likes
     `;
 
   const values = { your_id: your_id, diary_id: diary_id };
   const [result] = await conn.query(sql, values);
+
   return result as yourDiaryResults[];
 };
